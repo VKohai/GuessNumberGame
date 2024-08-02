58061c58206a69c0a1ddcb2dcba3fc3a89483251
VKohai <kohai.entertainment@gmail.com>
2024-08-02 14:33:24 +0300

    Add project files.

---
+++ GuessNumberGame/Program.cs
@@ -0,0 +1,18 @@
+﻿Random rand = new Random();
+var guessNumber = rand.Next(1, 101);
+Console.WriteLine("Бот загадал число. Отгадай");
+int myNumber = -1;
+
+while (myNumber != guessNumber)
+{
+    Console.Write("Я думаю, это число ");
+    myNumber = Int32.Parse(Console.ReadLine());
+    if (myNumber <= guessNumber)
+    {
+        Console.WriteLine("Число меньше заданного");
+    }
+    else if (myNumber >= guessNumber) {
+        Console.WriteLine("Число больше заданного");
+    }
+}
+Console.WriteLine($"Поздравляю, ты выйгриал! Это число {guessNumber}");
