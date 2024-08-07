d5e5873fa91b9793910deaba2d8015ba4865b460
VKohai <kohai.entertainment@gmail.com>
2024-08-07 10:04:02 +0300

    Sprint_02 is finished

--- GuessNumberGame/Program.cs
+++ GuessNumberGame/Program.cs
@@ -1,18 +1,26 @@
 ﻿Random rand = new Random();
-var guessNumber = rand.Next(1, 101);
-Console.WriteLine("Бот загадал число. Отгадай");
-int myNumber = -1;
-
-while (myNumber != guessNumber)
+while (true)
 {
-    Console.Write("Я думаю, это число ");
-    myNumber = Int32.Parse(Console.ReadLine());
-    if (myNumber <= guessNumber)
+    var guessNumber = rand.Next(1, 101);
+    Console.WriteLine("Бот загадал число. Отгадай");
+    int myNumber = -1;
+
+    while (myNumber != guessNumber)
     {
-        Console.WriteLine("Число меньше заданного");
+        Console.Write("Я думаю, это число ");
+        myNumber = Int32.Parse(Console.ReadLine());
+        if (myNumber <= guessNumber)
+        {
+            Console.WriteLine("Число меньше заданного");
+        } else if (myNumber >= guessNumber)
+        {
+            Console.WriteLine("Число больше заданного");
+        }
     }
-    else if (myNumber >= guessNumber) {
-        Console.WriteLine("Число больше заданного");
-    }
+    Console.WriteLine($"Поздравляю, ты выйгриал! Это число {guessNumber}");
+
+    Console.Write("Хочешь продолжить - нажми 1: ");
+    if (Console.ReadLine() != "1")
+        break;
+    Console.WriteLine();
 }
-Console.WriteLine($"Поздравляю, ты выйгриал! Это число {guessNumber}");
