5600eec0e176caedc86218c099651ae24543b20a
VKohai <kohai.entertainment@gmail.com>
2024-08-07 15:12:20 +0300

    Fix gramma

--- GuessNumberGame/Program.cs
+++ GuessNumberGame/Program.cs
@@ -17,7 +17,7 @@ while (true)
             Console.WriteLine("Число больше заданного");
         }
     }
-    Console.WriteLine($"Поздравляю, ты выйгриал! Это число {guessNumber}");
+    Console.WriteLine($"Поздравляю, ты выйграл! Это число {guessNumber}");
 
     Console.Write("Хочешь продолжить - нажми 1: ");
     if (Console.ReadLine() != "1")
